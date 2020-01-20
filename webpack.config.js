03a0949bfea85e87c6b626fdb16f238c3bd99eb1
Alexei Spirit <alexeispirit@gmail.com>
2020-01-20 18:43:06 +0500

    html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const HTMLWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
   mode: "development",
@@ -9,5 +10,10 @@ module.exports = {
   output: {
     filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "dist")
-  }
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    })
+  ]
 };
