362d385cc7c688394566834ddbdc97aed90b8197
Alexei Spirit <alexeispirit@gmail.com>
2020-01-21 23:40:21 +0500

    typescript support

--- webpack.config.js
+++ webpack.config.js
@@ -52,7 +52,7 @@ module.exports = {
   mode: "development",
   entry: {
     main: ["@babel/polyfill", "./index.js"],
-    analytics: "./analytics.js"
+    analytics: "./analytics.ts"
   },
   output: {
     filename: filename("js"),
@@ -125,6 +125,17 @@ module.exports = {
           loader: "babel-loader",
           options: {
             presets: ["@babel/preset-env"],
+            plugins: ["@babel/plugin-proposal-class-properties"]
+          }
+        }
+      },
+      {
+        test: /\.ts$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-typescript"],
             plugins: ["@babel/plugin-proposal-class-properties"]
           }
         }
