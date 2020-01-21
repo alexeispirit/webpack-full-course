1b87b921b53a84e71b508ba6241fbeabd61f9ec4
Alexei Spirit <alexeispirit@gmail.com>
2020-01-21 17:10:46 +0500

    csv loader

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import Post from "./Post";
 import json from "./assets/json.json";
-import "./styles/styles.css";
+import xml from "./assets/data.xml";
+import csv from "./assets/data.csv";
 import WebpackLogo from "./assets/webpack-logo.png";
+import "./styles/styles.css";
 
 const post = new Post("Webpack title", WebpackLogo);
 console.log("Post to String: ", post.toString());
 
 console.log("JSON: ", json);
+console.log("XML: ", xml);
+console.log("CSV: ", csv);
