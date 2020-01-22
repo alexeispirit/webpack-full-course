12fcc127363d02f3be35dec32c289571711802f6
Alexei Spirit <alexeispirit@gmail.com>
2020-01-22 13:14:51 +0500

    react support

--- webpack.config.js
+++ webpack.config.js
@@ -47,11 +47,24 @@ const cssLoaders = extra => {
   return loaders;
 };
 
+const babelOptions = preset => {
+  const options = {
+    presets: ["@babel/preset-env"],
+    plugins: ["@babel/plugin-proposal-class-properties"]
+  };
+
+  if (preset) {
+    options.presets.push(preset);
+  }
+
+  return options;
+};
+
 module.exports = {
   context: path.resolve(__dirname, "src"),
   mode: "development",
   entry: {
-    main: ["@babel/polyfill", "./index.js"],
+    main: ["@babel/polyfill", "./index.jsx"],
     analytics: "./analytics.ts"
   },
   output: {
@@ -123,10 +136,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
-          }
+          options: babelOptions()
         }
       },
       {
@@ -134,10 +144,15 @@ module.exports = {
         exclude: /node_modules/,
         loader: {
           loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env", "@babel/preset-typescript"],
-            plugins: ["@babel/plugin-proposal-class-properties"]
-          }
+          options: babelOptions("@babel/preset-typescript")
+        }
+      },
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        loader: {
+          loader: "babel-loader",
+          options: babelOptions("@babel/preset-react")
         }
       }
     ]
